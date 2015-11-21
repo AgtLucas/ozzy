e75ef8a308a96e2071210e3ab4de7ce67e43f7a6
Lucas <lucas@agtlucas.com>
2015-11-21 07:30:07 -0200

    404

--- scripts/main.js
+++ scripts/main.js
@@ -74,12 +74,22 @@ var StorePicker = React.createClass({
 
 });
 
+/* Not found */
+var NotFound = React.createClass({
+  render: function() {
+    return(
+      <h1>Not Found!</h1>
+    );
+  }
+});
+
 /* Routes */
 
 var routes = (
   <Router history={createBrowserHistory()}>
     <Route path="/" component={StorePicker} />
     <Route path="/store/:storeId" component={App} />
+    <Route path="*" component={NotFound} />
   </Router>
 );
 
