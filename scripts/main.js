3dff0c5400f70b506decce9cba32af069f05c8f3
Lucas <lucas@agtlucas.com>
2015-11-23 12:52:29 -0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -80,7 +80,7 @@ var App = React.createClass({
           </ul>
         </div>
         <Order fishes={this.state.fishes} order={this.state.order} />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState} removeFish={this.removeFish} />
       </div>
     );
 
@@ -233,7 +233,7 @@ var Inventory = React.createClass({
         </select>
         <textarea valueLink={linkState('fishes.' + key + '.desc')}></textarea>
         <input type="text" valueLink={linkState('fishes.'+ key +'.image')} />
-        <button>Remove Fish</button>
+        <button onClick={this.props.removeFish.bind(null, key)}>Remove Fish</button>
       </div>
     );
   },
